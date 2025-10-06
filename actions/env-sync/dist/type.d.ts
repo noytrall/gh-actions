bc7fbffd563eac8396ac66bdd38a94ae03b25273
fmcsilva <francisco.silva@techhub.cinch.co.uk>
2025-10-06 23:03:53 +0100

    rebuild

--- actions/env-sync/dist/type.d.ts
+++ actions/env-sync/dist/type.d.ts
@@ -1,49 +1,39 @@
-type BaseAwsResourceParameters<P> = {
-    region: string;
-    accessKeyId: string;
-    secretAccessKey: string;
-    sessionToken: string;
-} & P;
-type BaseDynamoParameters = BaseAwsResourceParameters<{
-    type: "dynamo";
-    dynamoTableName: string;
-}>;
-type BaseS3Parameters = BaseAwsResourceParameters<{
-    type: "s3";
-    s3BucketName: string;
-    s3Key: string;
-}>;
-type HttpRequestParameters = {
-    type: "http";
-    url: string;
-    httpMethod: string;
-    apiKey?: string;
-    apiKeyHeaderAttribute?: string;
-    apiKeyParamName?: string;
-};
-type Source = BaseDynamoParameters | BaseS3Parameters | HttpRequestParameters;
-type Target = (BaseDynamoParameters & ({
-    purgeTable: false;
-    tablePK?: string;
-    tableSK?: string;
-} | {
-    purgeTable: true;
-    tablePK: string;
-    tableSK?: string;
-})) | BaseS3Parameters | HttpRequestParameters;
-export type Config = {
-    source: Source;
-    target: Target;
-    piiStuff?: boolean;
-    dataTransformer?: {
-        type: "http";
-        httpMethod: string;
-        endpointUrl: string;
-        apiKey?: string;
-        apiKeyHeaderAttribute?: string;
-        apiKeyParamName?: string;
-    } | {
-        type: "function";
-    };
-};
-export {};
+import z from "zod";
+export declare const configSchema: z.ZodObject<{
+    source: z.ZodDiscriminatedUnion<[z.ZodObject<{
+        region: z.ZodString;
+        accessKeyId: z.ZodString;
+        secretAccessKey: z.ZodString;
+        sessionToken: z.ZodString;
+        type: z.ZodLiteral<"dynamo">;
+        dynamoTableName: z.ZodString;
+    }, z.z.core.$strip>, z.ZodObject<{
+        region: z.ZodString;
+        accessKeyId: z.ZodString;
+        secretAccessKey: z.ZodString;
+        sessionToken: z.ZodString;
+        type: z.ZodLiteral<"s3">;
+        s3BucketName: z.ZodString;
+        s3Key: z.ZodString;
+    }, z.z.core.$strip>], "type">;
+    target: z.ZodDiscriminatedUnion<[z.ZodObject<{
+        region: z.ZodString;
+        accessKeyId: z.ZodString;
+        secretAccessKey: z.ZodString;
+        sessionToken: z.ZodString;
+        type: z.ZodLiteral<"dynamo">;
+        dynamoTableName: z.ZodString;
+        purgeTable: z.ZodBoolean;
+        tablePK: z.ZodOptional<z.ZodString>;
+        tableSK: z.ZodOptional<z.ZodString>;
+    }, z.z.core.$strip>, z.ZodObject<{
+        region: z.ZodString;
+        accessKeyId: z.ZodString;
+        secretAccessKey: z.ZodString;
+        sessionToken: z.ZodString;
+        type: z.ZodLiteral<"s3">;
+        s3BucketName: z.ZodString;
+        s3Key: z.ZodString;
+    }, z.z.core.$strip>], "type">;
+}, z.z.core.$strip>;
+export type Config = z.infer<typeof configSchema>;
