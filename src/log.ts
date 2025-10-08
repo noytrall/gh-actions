c4824f646ebb6994ffb7b6694a064d1a188f322f
fmcsilva <francisco.silva@techhub.cinch.co.uk>
2025-10-08 17:38:56 +0100

    testing

--- src/log.ts
+++ src/log.ts
@@ -3,6 +3,18 @@ import { getErrorMessage } from "./utils/errors.js";
 
 async function run() {
   try {
+    const sourceAwsAccessKeyId = core.getInput("source-aws-access-key-id", {
+      required: true,
+    });
+    const sourceAwsSecretAccessKey = core.getInput(
+      "source-aws-secret-access-key",
+      {
+        required: true,
+      }
+    );
+    const sourceAwsSessionToken = core.getInput("source-aws-session-token", {
+      required: true,
+    });
     const targetAwsAccessKeyId = core.getInput("target-aws-access-key-id", {
       required: true,
     });
@@ -16,11 +28,25 @@ async function run() {
       required: true,
     });
 
-    console.log("ENV", JSON.stringify(process.env, null, 2));
+    console.log(
+      "ENV",
+      JSON.stringify(
+        Object.fromEntries(
+          Object.entries(process.env).map(([key, value]) => {
+            return [key, value?.split("")];
+          })
+        ),
+        null,
+        2
+      )
+    );
 
     console.log("targetAwsAccessKeyId :>> ", targetAwsAccessKeyId);
     console.log("targetAwsSecretAccessKey :>> ", targetAwsSecretAccessKey);
     console.log("targetAwsSessionToken :>> ", targetAwsSessionToken);
+    console.log("sourceAwsAccessKeyId :>> ", sourceAwsAccessKeyId);
+    console.log("sourceAwsSecretAccessKey :>> ", sourceAwsSecretAccessKey);
+    console.log("sourceAwsSessionToken :>> ", sourceAwsSessionToken);
   } catch (error) {
     core.setFailed(getErrorMessage(error));
   }
