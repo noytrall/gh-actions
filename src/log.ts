47a0483c999c72fe102b991baa8741e36ebe5af5
fmcsilva <francisco.silva@techhub.cinch.co.uk>
2025-10-08 17:31:49 +0100

    update stuff

--- src/log.ts
+++ src/log.ts
@@ -3,7 +3,24 @@ import { getErrorMessage } from "./utils/errors.js";
 
 async function run() {
   try {
+    const targetAwsAccessKeyId = core.getInput("target-aws-access-key-id", {
+      required: true,
+    });
+    const targetAwsSecretAccessKey = core.getInput(
+      "target-aws-secret-access-key",
+      {
+        required: true,
+      }
+    );
+    const targetAwsSessionToken = core.getInput("target-aws-session-token", {
+      required: true,
+    });
+
     console.log("ENV", JSON.stringify(process.env, null, 2));
+
+    console.log("targetAwsAccessKeyId :>> ", targetAwsAccessKeyId);
+    console.log("targetAwsSecretAccessKey :>> ", targetAwsSecretAccessKey);
+    console.log("targetAwsSessionToken :>> ", targetAwsSessionToken);
   } catch (error) {
     core.setFailed(getErrorMessage(error));
   }
